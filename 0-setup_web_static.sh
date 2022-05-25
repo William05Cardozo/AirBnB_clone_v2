4255fa958f7517bfa9bb8a1265b5e1cef6c5f694
William05Cardozo <3784@holbertonschool.com>
2022-05-25 12:54:06 -0700

    Task 0: Setup_Web_Static_CorrectionCode

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /dara/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
 echo "<!DOCTYPE html>
 <html>
