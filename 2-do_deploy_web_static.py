36b30a9841a7c9aecc2e703c66402fbfb33cbe5f
William05Cardozo <3784@holbertonschool.com>
2022-05-25 15:58:40 -0700

    Task 2: Do_Deploy_Static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """
-script that distributes archive to webservers
+Fabric script that generates a .tgz archive from the
+contents of the web_static folder of your AirBnB Clone
 """
-from os.path import exists
-from fabric.api import put, run, env
-env.hosts = ['34.138.174.14', '18.207.116.48']
+from os import *
+from fabric.api import *
+env.user = 'ubuntu'
+env.hosts = ['35.237.1.254', '54.197.195.161']
 
 
 def do_deploy(archive_path):
-    """ deploy """
-    if exists(archive_path) is False:
+    """distributes an archive to your web servers"""
+    if archive_path is None:
         return False
-
     try:
-        new = archive_path.split("/")[-1]
-        new_folder = new.split(".")[0]
+        nfile = archive_path.split("/")[-1]
+        npath = nfile.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}{}".format(path, new_folder))
-        run("tar xzf /tmp/{} -C {}{}".format(new, path, new_folder))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, new_folder))
-        run("rm /tmp/{}".format(new))
-        run("rm /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, new_folder))
-        return True
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, npath))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(nfile, path, npath))
+        run('sudo rm /tmp/{}'.format(nfile))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, npath))
+        run('sudo rm -rf {}{}/web_static'.format(path, npath))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, npath))
     except Exception:
         return False
+    return True
