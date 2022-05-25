0994c2b8c92ce53fdd8272333fa8a0eb6c7da90a
William05Cardozo <3784@holbertonschool.com>
2022-05-25 00:30:46 -0700

    Task 0: Hello_Route_CorrectionCode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    """return Hello HBNB!"""
+    return("Hello HBNB!")
 
 
 if __name__ == '__main__':
