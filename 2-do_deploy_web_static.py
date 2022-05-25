c3b66664758af5ef1676679a0fe8568b0dcbc8ff
William05Cardozo <3784@holbertonschool.com>
2022-05-25 14:05:07 -0700

    Task 1: Pack_Web_static_CorrectionCode

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,8 +9,8 @@ env.hosts = ['34.138.174.14', '18.207.116.48']
 
 def do_deploy(archive_path):
     """ deploy """
-    if exists((archive_path) is False):
-        return(False)
+    if exists(archive_path) is False:
+        return False 
 
     try:
         new = archive_path.split("/")[-1]
@@ -18,12 +18,11 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
         run("mkdir -p {}{}".format(path, new_folder))
-        run("tar xzf /tmp/{} -C {}{}".
-            format(new, path, new_folder))
-        rm("mv {0}{1}/web_static/* {0}{1}/".format(path, new_folder))
+        run("tar xzf /tmp/{} -C {}{}".format(new, path, new_folder))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, new_folder))
         run("rm /tmp/{}".format(new))
         run("rm /data/web_static/current")
         run("ln -s {}{}/ /data/web_static/current".format(path, new_folder))
-        return(True)
+        return True
     except Exception:
-        return(Falsie)
+        return False
