f3a0c5ee5ed32f4b70158b9fbcb4947e164341ea
William05Cardozo <3784@holbertonschool.com>
2022-05-25 13:28:28 -0700

    Task 1: Pack_Web_static_CorrectionCode

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,29 +2,28 @@
 """
 script that distributes archive to webservers
 """
-import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
+from os.path import exists
+from fabric.api import put, run, env
 env.hosts = ['34.138.174.14', '18.207.116.48']
 
 
 def do_deploy(archive_path):
     """ deploy """
-    if (os.path.isfile(archive_path) is False):
+    if exists(archive_path) is False):
         return(False)
 
     try:
         new = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new.split(".")[0])
+        new_folder = new.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new, new_folder))
-        run("sudo rm /tmp/{}".format(new))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        run("mkdir -p {}{}".format(path, new_folder))
+        run("tar xzf /tmp/{} -C {}{}".
+            format(new, path, new_folder))
+        rm("mv {0}{1}/web_static/* {0}{1}/".format(path, new_folder))
+        run("rm /tmp/{}".format(new))
+        run("rm /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(path, new_folder))
         return(True)
     except Exception:
         return(False)
