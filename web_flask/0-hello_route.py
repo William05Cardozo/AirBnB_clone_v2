9b5aede5ac56682da3c137c3dbfbb6c994b53202
William05Cardozo <3784@holbertonschool.com>
2022-05-24 23:49:51 -0700

    Web_FLASK, Task 0: Hello_Route, README

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-""" 
-Write a script that start a FlaskWeb application
-Listening on 0.0.0.0 in Port: 5000
-"""
-from flask import Flask
+
+from flask import *
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_HBNB():
-    """return a message"""
-    return "Hello HBNB!!"
+def flask_0():
+    return(“Hello HBNB!”)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
-  
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=500i0, debug=True)
--- web_flask/README.md
+++ web_flask/README.md
@@ -1,1 +1,5 @@
-This is a Readme for Subdirectory `WebFlask`
+Project Web FLASK
+Start: 1:27am - 24/05/2.022
+
+In this part of the AirBNB_V2, we using the Framework FLASK.
+
