a912365700d886f66860a615a7c2b737be237d67
William05Cardozo <3784@holbertonschool.com>
2022-05-24 21:46:00 -0700

    Task 2: Do_Deply_Static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 """
-This script distributes an archive to your web servers.
+script that distributes archive to webservers
 """
-
 import os.path
 from fabric.api import *
 from fabric.operations import run, put, sudo
-env.host = ['34.138.174.14', '18.207.116.48']
+env.hosts = ['34.138.174.14', '18.207.116.48']
 
 
 def do_deploy(archive_path):
-    if (os.path.isfile(archive.path) is False):
+    """ deploy """
+    if (os.path.isfile(archive_path) is False):
         return(False)
+
     try:
         new = archive_path.split("/")[-1]
         new_folder = ("/data/web_static/releases/" + new.split(".")[0])
-        put(archive_path, "/tmp")
+        put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(new_folder))
         run("sudo tar -xzf /tmp/{} -C {}".
             format(new, new_folder))
         run("sudo rm /tmp/{}".format(new))
         run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
         run("sudo rm -rf {}/web_static".format(new_folder))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ls -s {} /data/web_static/current".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
         return(True)
     except Exception:
         return(False)
