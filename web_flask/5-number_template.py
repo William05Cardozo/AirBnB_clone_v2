16ff5b5d83b76edbce661ef6fda4d12f9b1fb0a1
William05Cardozo <3784@holbertonschool.com>
2022-05-25 19:01:07 -0700

    Task 5: Number_Template, Directory: Template_with_HTMLPage

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Start a FLASK in a Web Aplication
+add a new route: /hbnb
+add a new route: /Python
+add a new route: /number
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns Hello HBNB!"""
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnh():
+    """return HBNB"""
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    """Return the letter C"""
+    text = text.replace("_", " ")
+    return("C {}".format(text))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def display_Python(text="is cool"):
+    """Return the String"""
+    text2 = text.replace("_", " ")
+    return("Python {}".format(text2))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def Is_Number(n):
+    """Return if a parameter is a number"""
+    return("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temp(n):
+    """Return a HTML PAge if the paramater is a number"""
+    return(render_template('5-number.html', num=n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
