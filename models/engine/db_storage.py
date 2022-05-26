b0943d8c58ca472ef2e34606665ab1a3c9708f1f
William05Cardozo <3784@holbertonschool.com>
2022-05-25 22:07:35 -0700

    Task 7: File_Storage, DB_Storage, State

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,3 +72,7 @@ class DBStorage:
                                        expire_on_commit=False)
         self.__session = scoped_session(
             session_factory)
+
+    def close(self):
+        """Function, call remove()"""
+        self.__session.close()
